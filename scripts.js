1dafea3f183c932f32b872d086356f9cc5ae1bdd
Evg-Kir <kirikoy1@gmail.com>
2018-01-29 22:58:55 +0200

    +

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+$ (document).ready(function() {
+
+	$('.post').click(function() {
+		$(this).toggleClass('post_active');
+	});
+	$('.menu_button').click(function() {
+		$(this).toggleClass('menu_active');
+	});
+
+});
+
+
